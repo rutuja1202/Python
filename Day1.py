10b80c791f4516d74eef614abd59f6a7ae736d03
rutuja1202 <patharerutuja1202@gmail.com>
2025-11-07 14:43:27 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,10 @@
+print("Welcome to python programming language!")
+print(30 * 40)
+
+
+
+import turtle;
+t=turtle.Turtle()
+for i in range(40):
+    t.forward(100)
+    t.right(90)
---
+++ Day2.py
@@ -0,0 +1,39 @@
+#Variable 
+print("Variables in Python")
+
+skill = "full stack developer"
+print("My skill is:", skill)
+
+#****************************************
+# assign multiple values in one line
+x, y, z = 10, 20.5, "Hello"
+print(x, y, z)
+# ********************************************
+# assign one valu to multiple variables
+a = b = c = 50
+print(a, b, c)
+# ********************************************
+
+# Data Types
+# integer
+age=20
+print("My age is:", age)
+
+# type()-check the type of variable
+print("The type of age variable is:", type(age))
+
+# float
+price=99.99
+print("The price is:", price)
+
+# boolean-use to check condition its true or false
+isEligible=True
+print("Is Eligible:", isEligible)
+
+# String 
+name="Rutuja"
+print("My name is:", name)
+
+# null value
+data=None
+print("The data is:", data)
